1c339985cf758c132fe036e51f6663df40c17512
Somnath Roy <somnathroy0340@gmail.com>
2024-03-29 18:49:37 +0530

    Update

--- server/mail/templates/courseEnrollmentEmail.js
+++ server/mail/templates/courseEnrollmentEmail.js
@@ -67,7 +67,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
   
   <body>
       <div class="container">
-          <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" 
+          <a href="https://skill-teach-somnath-roy.vercel.app/"><img class="logo" 
           src="https://i.ibb.co/df3xLtN/Skill-Teach-logos-1.jpg"
           alt="Skill Teach Logo"></a>
           <div class="message">Course Registration Confirmation</div>
@@ -77,7 +77,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
                   are excited to have you as a participant!</p>
               <p>Please log in to your learning dashboard to access the course materials and start your learning journey.
               </p>
-              <a class="cta" href="https://studynotion-edtech-project.vercel.app/dashboard">Go to Dashboard</a>
+              <a class="cta" href="https://skill-teach-somnath-roy.vercel.app/dashboard">Go to Dashboard</a>
           </div>
           <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
                   href="mailto:info@skillTeach.com">info@skillTeach.com</a>. We are here to help!</div>
